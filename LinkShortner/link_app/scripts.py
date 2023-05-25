7fb709cd76b16afdaed2ef728f6d0850cc3c9a98
Naman-0206 <edu.ngarg@gmail.com>
2023-05-25 16:03:12 +0530

    removed scripts & added server task

--- LinkShortner/link_app/scripts.py
+++
@@ -1,7 +0,0 @@
-from .models import Urls
-from datetime import datetime , timedelta
-
-def delete_one_month_old_data():
-    cutoff_date = datetime.now().date() - timedelta(days=30)
-    Urls.objects.filter(created_at__lte=cutoff_date)
-    #print("Data Deleted Succesfully!")
